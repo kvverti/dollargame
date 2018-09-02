26ab90965056de88bc84524aebfc37bd16867c5f
Chris Nero <chrisvn00@gmail.com>
2018-09-02 16:12:34 -0400

    Add game options and UI responses

--- graph.js
+++ graph.js
@@ -28,9 +28,16 @@ var drag = d3.drag()
 //currently selected node
 var selected = null;
 
+d3.select("svg")
+    .call(d3.zoom()
+        .on("zoom", function() {
+            graph.attr("transform", d3.event.transform);
+        }))
+    .on("dblclick.zoom", null);
+
 //generates edges to form a connected graph and sets the initial counts
 //for each node. The total count is at least as large as the genus.
-function generateNewEdges(nodes) {
+function generateNewEdges(nodes, surplus, range) {
     //generate the edge data. We want a connected graph, so
     //we keep an array of the nodes not in the connected segment,
     //removing them when we connect an edge between them.
@@ -47,8 +54,10 @@ function generateNewEdges(nodes) {
     }
     //add a few cycles in
     for(var i = 0; i < nodes.length / 3; i++) {
-        var node1 = nodes[Math.floor(Math.random() * nodes.length)];
+        var node1;
         var node2;
+        do { node1 = nodes[Math.floor(Math.random() * nodes.length)]; }
+        while(node1.adj.length === nodes.length - 1);
         do { node2 = nodes[Math.floor(Math.random() * nodes.length)]; }
         while(node1.id === node2.id || node1.adj.find(el => el.id === node2.id));
         node1.adj.push(node2);
@@ -58,7 +67,7 @@ function generateNewEdges(nodes) {
     var genus = edges.length - nodes.length + 1;
     //assign an initial value
     for(var idx in nodes) {
-        var count = Math.floor(Math.random() * 20) - 10;
+        var count = Math.floor(Math.random() * range * 2) - range;
         nodes[idx].count = count;
         genus -= count;
     }
@@ -66,15 +75,23 @@ function generateNewEdges(nodes) {
     while(genus-- > 0) {
         nodes[Math.floor(Math.random() * nodes.length)].count++;
     }
+    //make the total count exactly equal to the genus for extra challenge
+    if(!surplus) {
+        console.log(surplus);
+        while(++genus < 0) {
+            nodes[Math.floor(Math.random() * nodes.length)].count--;
+        }
+    }
     return edges;
 }
 
-function generateGraph(num) {
+function generateGraph(num, surplus, range) {
     //generate the node data
     sim.stop();
+    resetStats();
     var newData = Array.from(Array(num)).map((e, i) => ({ id: i, count: 0, adj: [] }));
-    var newEdges = generateNewEdges(newData);
-    //update edges
+    var newEdges = generateNewEdges(newData, surplus, range);
+    //generate edges
     edges = graph.selectAll(".graph-edge")
         .data(newEdges);
     edges.exit().remove();
@@ -82,7 +99,7 @@ function generateGraph(num) {
       .append("line")
         .attr("class", "graph-edge")
       .merge(edges);
-    //update nodes
+    //generate nodes
     nodes = graph.selectAll(".graph-node")
         .data(newData);
     nodes.exit().remove();
@@ -90,11 +107,15 @@ function generateGraph(num) {
       .append("g")
         .on("click", handleClick)
         .on("contextmenu", handleContext)
+        .on("mouseover", handleHover)
+        .on("mouseout", handleHoverEnd)
         .attr("class", "graph-node")
         .call(sel => sel.append("circle"))
         .call(sel => sel.append("text"))
       .merge(nodes)
-        .call(drag);
+        .call(drag)
+        .call(updateText)
+        .order();
     linkForce.links(newEdges);
     var rect = graph.node().parentNode.getBoundingClientRect();
     centerForce.x((rect.right - rect.left) / 2);
@@ -103,9 +124,7 @@ function generateGraph(num) {
 }
 
 function updateGraph() {
-    nodes.attr("transform", d => "translate(" + d.x + "," + d.y + ")")
-      .select("text")
-        .text(d => d.count);
+    nodes.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
     edges.attr("x1", d => d.source.x)
         .attr("y1", d => d.source.y)
         .attr("x2", d => d.target.x)
@@ -116,6 +135,8 @@ function handleClick(d) {
     //give to adjacent
     d.count -= d.adj.length;
     d.adj.forEach(el => el.count++);
+    updateText(nodes);
+    updateStats();
 }
 
 function handleContext(d) {
@@ -123,6 +144,59 @@ function handleContext(d) {
     //take from adjacent
     d.count += d.adj.length;
     d.adj.forEach(el => el.count--);
+    updateText(nodes);
+    updateStats();
 }
 
-generateGraph(15);
+function updateText(sel) {
+    sel.select("text")
+        .text(d => d.count)
+        .classed("negative", d => d.count < 0);
+}
+
+var statMoves = document.getElementById("stat-moves");
+function updateStats() {
+    statMoves.innerHTML++;
+}
+
+function resetStats() {
+    statMoves.innerHTML = 0;
+}
+
+function handleHover(d) {
+    var affected = nodes.filter(d1 => d1 === d || d.adj.indexOf(d1) >= 0);
+    affected.append("circle")
+        .attr("class", "outline");
+    edges.filter(d1 =>
+            (d1.source === d || d1.target === d)
+         && affected.data().indexOf(d1.source) >= 0
+         && affected.data().indexOf(d1.target) >= 0)
+        .classed("highlighted", true);
+}
+
+function handleHoverEnd(d) {
+    var affected = nodes.filter(d1 => d1 === d || d.adj.indexOf(d1) >= 0);
+    affected.select(".outline")
+      .remove();
+    edges.filter(d1 =>
+            (d1.source === d || d1.target === d)
+         && affected.data().indexOf(d1.source) >= 0
+         && affected.data().indexOf(d1.target) >= 0)
+        .classed("highlighted", false);
+}
+
+//input event handlers
+
+var inputNodeCount = document.getElementById("input-node-count");
+var inputSurplus = document.getElementById("input-surplus");
+var inputRange = document.getElementById("input-range");
+var generate = document.getElementById("input-generate").onclick = function() {
+    var nodeCount = parseInt(inputNodeCount.value, 10);
+    var allowSurplus = inputSurplus.checked;
+    var range = parseInt(inputRange.value, 10);
+    if(nodeCount && nodeCount >= 5 && range && range >= 1) {
+        generateGraph(nodeCount, allowSurplus, range);
+    }
+};
+
+generate();
